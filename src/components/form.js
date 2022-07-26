85cde6ba8912891c9a257f7ab6f065dd3758ecfe
Harsh-910 <97285448+Harsh-910@users.noreply.github.com>
2022-07-26 16:23:18 +0530

    select

--- src/components/form.js
+++ src/components/form.js
@@ -1,11 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
+import Select from "react-select";
 
 const CreateForm = () => {
+  const [selectedValue, SetselectedValue] = useState("");
+  const age = [
+    {
+      value: "15",
+      label: "15"
+    },
+    {
+      value: "17",
+      label: "17"
+    }
+  ];
+
   const validationSchema = yup.object({
     email: yup
       .string("Enter your email")
@@ -14,20 +26,28 @@ const CreateForm = () => {
     password: yup
       .string("Enter your password")
       .min(8, "Password should be of minimum 8 characters length")
-      .required("Password is required")
+      .required("Password is required"),
+    age: yup.string("Enter Age").required("Age Is Required")
   });
 
   const formik = useFormik({
     initialValues: {
       email: "foobar@example.com",
-      password: "foobar"
+      password: "foobar",
+      age: ""
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     }
   });
+  useEffect(() => {
+    formik.values.email = "ddf@gmail.com";
+  }, [formik.values]);
 
+  const handleage = (selected) => {
+    SetselectedValue(selected);
+  };
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
@@ -51,6 +71,13 @@ const CreateForm = () => {
           onChange={formik.handleChange}
           error={formik.touched.password && Boolean(formik.errors.password)}
           helperText={formik.touched.password && formik.errors.password}
+        />
+        <Select
+          value={formik.values.age}
+          options={age}
+          onChange={formik.handleChange}
+          error={formik.touched.age && Boolean(formik.errors.age)}
+          helperText={formik.touched.age && formik.errors.age}
         />
         <Button color="primary" variant="contained" fullWidth type="submit">
           Submit

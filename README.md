e292abce630b76471b68508ce5f9ae77ad5ea238
Harsh-910 <harshchhabra910@gmail.com>
2022-07-26 14:41:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tp9u2p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tp9u2p)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import Form from './components/form';
+
+export default function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
